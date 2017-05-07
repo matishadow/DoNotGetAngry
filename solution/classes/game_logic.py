36610ea9f33eee0b686d7c725f7c66803e61eebc
matishadow <matishadow@gmail.com>
2017-05-07 20:32:34 +0200

    Add first move logic

--- solution/classes/game_logic.py
+++ solution/classes/game_logic.py
@@ -3,12 +3,20 @@ from enum import Enum
 from itertools import cycle
 
 
+def player_has_all_counters_in_starting_position(player):
+    return len(player.starting_tiles) == player.SPECIAL_TILES_COUNT
+
+
+def throw_was_maximum(throw):
+    return throw == Game.MAXIMUM_NUMBER_OF_DOTS
+
+
 class Game:
     MINIMAL_NUMBER_OF_PLAYERS = 2
     ALL_COUNTERS_IN_STARTING_THROWS = 3
-    NORMAL_THROWS = 1
     MAXIMUM_NUMBER_OF_DOTS = 6
-
+    NORMAL_THROWS = 1
+    MAXIMUM_THROW_RETRIES = 3
 
     def __init__(self, number_of_players):
         if number_of_players < self.MINIMAL_NUMBER_OF_PLAYERS:
@@ -18,20 +26,45 @@ class Game:
         self.color_cycle = cycle(self.colors)
         self.current_color = next(self.color_cycle)
         self.players = [Player(color) for color in self.colors]
+        self.board = Board(number_of_players)
 
-    def turn(self):
-        current_player = self.players[self.current_color.value]
+    def turn(self, user_decision_callback):
+        player_index = self.current_color.value
+        current_player = self.players[player_index]
 
-        if len(current_player.starting_tiles) == current_player.SPECIAL_TILES_COUNT:
+        if player_has_all_counters_in_starting_position(current_player):
             for i in range(self.ALL_COUNTERS_IN_STARTING_THROWS):
                 throw = Dice.throw_the_dice()
 
-                if throw == self.MAXIMUM_NUMBER_OF_DOTS:
+                if throw_was_maximum(throw):
+                    counter_index = self.board.bring_out_counter(current_player, player_index)
+
+                    throw = Dice.throw_the_dice()
+                    if throw_was_maximum(throw):
+                        counter_index = self.board.move_counter(counter_index, throw)
+
+                        throw = Dice.throw_the_dice()
+                        if throw_was_maximum(throw):
+                            decision = user_decision_callback()
+                            if decision == UserDecision.OUT.name:
+                                self.board.bring_out_counter(current_player, player_index)
+                                break
+
+                            elif decision == UserDecision.MOVE.name:
+                                self.board.move_counter(counter_index, throw)
+                                break
+                            else:
+                                raise Exception("User input is not valid. Use 'OUT[0] or MOVE[1]'")
+                        else:
+                            self.board.move_counter(counter_index, throw)
+                            break
+                    else:
+                        self.board.move_counter(counter_index, throw)
+                        break
 
         else:
             for i in range(self.NORMAL_THROWS):
                 throw = Dice.throw_the_dice()
-
 
         self.current_color = next(self.color_cycle)
 
@@ -41,6 +74,11 @@ class Color(Enum):
     GREEN = 1
     BLUE = 2
     YELLOW = 3
+
+
+class UserDecision(Enum):
+    OUT = 0
+    MOVE = 1
 
 
 class Counter:
@@ -58,11 +96,28 @@ class Player:
 
 
 class Board:
+
     TILES_COUNT = 40
+    TILES_PER_PLAYER = 10
 
     def __init__(self, number_of_players):
         self.number_of_players = number_of_players
         self.tiles = [None] * self.TILES_COUNT
+
+    def move_counter(self, current_position, offset):
+        counter = self.tiles[current_position]
+        self.tiles[current_position] = None
+        new_position = current_position + offset
+        self.tiles[new_position] = counter
+
+        return new_position
+
+    def bring_out_counter(self, player, player_index):
+        counter = player.starting_tiles.pop()
+        counter_index = player_index * self.TILES_PER_PLAYER
+        self.tiles[counter_index] = counter
+
+        return counter_index
 
 
 class Dice:
