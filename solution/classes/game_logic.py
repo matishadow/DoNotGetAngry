1c9b6e842590c814c84318dc36c936c8403c8d09
matishadow <matishadow@gmail.com>
2017-05-08 22:44:11 +0200

    Add eliminating counters

--- solution/classes/game_logic.py
+++ solution/classes/game_logic.py
@@ -37,29 +37,28 @@ class Game:
                 throw = Dice.throw_the_dice()
 
                 if throw_was_maximum(throw):
-                    counter_index = self.board.bring_out_counter(current_player)
+                    counter_index = self.board.bring_out_counter(current_player, self.players)
 
                     throw = Dice.throw_the_dice()
                     if throw_was_maximum(throw):
-                        counter_index = self.board.move_counter(counter_index, throw)
+                        counter_index = self.board.move_counter(counter_index, throw, self.players)
 
                         throw = Dice.throw_the_dice()
                         if throw_was_maximum(throw):
                             decision = user_decision_callback()
                             if decision == UserDecision.OUT.name:
-                                self.board.bring_out_counter(current_player)
+                                self.board.bring_out_counter(current_player, self.players)
                                 break
-
                             elif decision == UserDecision.MOVE.name:
-                                self.board.move_counter(counter_index, throw)
+                                self.board.move_counter(counter_index, throw, self.players)
                                 break
                             else:
                                 raise Exception("User input is not valid. Use 'OUT[0] or MOVE[1]'")
                         else:
-                            self.board.move_counter(counter_index, throw)
+                            self.board.move_counter(counter_index, throw, self.players)
                             break
                     else:
-                        self.board.move_counter(counter_index, throw)
+                        self.board.move_counter(counter_index, throw, self.players)
                         break
 
         else:
@@ -71,13 +70,13 @@ class Game:
                 if throw_was_maximum(throw):
                     decision = user_decision_callback()
                     if decision == UserDecision.OUT.name:
-                        self.board.bring_out_counter(current_player)
+                        self.board.bring_out_counter(current_player, self.players)
                     elif decision == UserDecision.MOVE.name:
-                        self.board.move_when_out(current_player, throw, user_counter_chosen_callback)
+                        self.board.move_when_out(current_player, throw, user_counter_chosen_callback, self.players)
                     else:
                         raise Exception("User input is not valid. Use 'OUT[0] or MOVE[1]'")
                 else:
-                    self.board.move_when_out(current_player, throw, user_counter_chosen_callback)
+                    self.board.move_when_out(current_player, throw, user_counter_chosen_callback, self.players)
 
                 if throw_count > self.MAXIMUM_THROW_RETRIES or throw != self.MAXIMUM_NUMBER_OF_DOTS:
                     break
@@ -110,7 +109,7 @@ class Player:
 
     def __init__(self, color):
         self.color = color
-        self.starting_tiles = self.SPECIAL_TILES_COUNT * [Counter(color)]
+        self.starting_tiles = [Counter(color) for _ in range(self.SPECIAL_TILES_COUNT)]
         self.home_tiles = []
         self.on_board_counters = []
         self.first_index_on_board = color.value * Board.TILES_PER_PLAYER
@@ -124,22 +123,22 @@ class Board:
         self.number_of_players = number_of_players
         self.tiles = [None] * self.TILES_COUNT
 
-    def check_eliminate(self):
-        return True
-
-    def move_counter(self, current_position, offset):
+    def move_counter(self, current_position, offset, players):
         counter = self.tiles[current_position]
         self.tiles[current_position] = None
         new_position = current_position + offset
+
+        self.try_eliminate(players, counter, new_position)
+
         self.tiles[new_position] = counter
         counter.position = new_position
 
         return new_position
 
-    def move_when_out(self, current_player, throw, user_counter_chosen_callback):
+    def move_when_out(self, current_player, throw, user_counter_chosen_callback, players):
         if len(current_player.on_board_counters) == 1:
             [counter] = current_player.on_board_counters
-            self.move_counter(counter.position, throw)
+            self.move_counter(counter.position, throw, players)
         else:
             counter_to_move_index = user_counter_chosen_callback()
             counter = self.tiles[counter_to_move_index]
@@ -148,15 +147,41 @@ class Board:
             if counter.color != current_player.color:
                 raise Exception("This is not your counter!")
 
-            self.move_counter(counter.position, throw)
+            self.move_counter(counter.position, throw, players)
 
-    def bring_out_counter(self, player):
-        counter = player.starting_tiles.pop()
-        counter_index = player.first_index_on_board
+    def check_eliminate(self, moving_color, position):
+        counter_on_position = self.tiles[position]
+        if counter_on_position is not None:
+            if counter_on_position.color != moving_color:
+                return True
+            else:
+                raise Exception("You cannot eliminate your own counter")
+        else:
+            return False
+
+    def eliminate_counter(self, players, position):
+        counter_to_eliminate = self.tiles[position]
+        counter_to_eliminate.position = Counter.INITIAL_STARTING_POSITION
+        self.tiles[position] = None
+        player = players[counter_to_eliminate.color.value]
+        player.starting_tiles.append(counter_to_eliminate)
+        player.on_board_counters.remove(counter_to_eliminate)
+
+    def try_eliminate(self, players, counter, counter_index):
+        is_eliminating = self.check_eliminate(counter.color, counter_index)
+        if is_eliminating:
+            self.eliminate_counter(players, counter_index)
+
+    def bring_out_counter(self, current_player, players):
+        counter = current_player.starting_tiles.pop()
+        counter_index = current_player.first_index_on_board
+
+        self.try_eliminate(players, counter, counter_index)
+
         counter.position = counter_index
 
         self.tiles[counter_index] = counter
-        player.on_board_counters.append(counter)
+        current_player.on_board_counters.append(counter)
 
         return counter_index
 
