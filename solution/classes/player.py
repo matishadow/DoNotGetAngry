f718b93f74010c6e94488d4b82f12a8b4f7c6d69
matishadow <matishadow@gmail.com>
2017-05-10 09:18:27 +0200

    Add last_index_on_board

--- solution/classes/player.py
+++ solution/classes/player.py
@@ -8,6 +8,7 @@ class Player:
     def __init__(self, color):
         self.color = color
         self.starting_tiles = [Counter(color) for _ in range(self.SPECIAL_TILES_COUNT)]
-        self.home_tiles = []
+        self.home_tiles = [None] * self.SPECIAL_TILES_COUNT
         self.on_board_counters = []
         self.first_index_on_board = color.value * Board.TILES_PER_PLAYER
+        self.last_index_on_board = (self.first_index_on_board + (Board.TILES_COUNT - 1)) % Board.TILES_COUNT
