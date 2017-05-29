3a309943297e01d8a737971423de3548491b554f
matishadow <matishadow@gmail.com>
2017-05-29 12:39:10 +0200

    dice object into Game

--- solution/classes/dice.py
+++ solution/classes/dice.py
@@ -4,9 +4,14 @@ from random import randint
 class Dice:
     MAXIMUM_NUMBER_OF_DOTS = 6
 
-    @staticmethod
-    def throw_the_dice():
-        return randint(1, 6)
+    def __init__(self):
+        self.last_throw = -1
+
+    def throw_the_dice(self):
+        throw = randint(1, 6)
+        self.last_throw = throw
+
+        return throw
 
     @staticmethod
     def throw_was_maximum(throw):
