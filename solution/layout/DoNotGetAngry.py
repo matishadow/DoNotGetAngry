fdef07023bf31f89c65b0af60dfcdb76eff2f994
matishadow <matishadow@gmail.com>
2017-06-10 15:18:25 +0200

    update python code from layout

--- solution/layout/DoNotGetAngry.py
+++ solution/layout/DoNotGetAngry.py
@@ -455,13 +455,6 @@ class Ui_Dialog(object):
         self.blue_home_tile_0.setText(_fromUtf8(""))
         self.blue_home_tile_0.setObjectName(_fromUtf8("blue_home_tile_0"))
 
-        self.dice = QtGui.QPushButton(Dialog)
-        self.dice.setGeometry(QtCore.QRect(1000, 410, 75, 81))
-        self.dice.setStyleSheet(_fromUtf8("background-color:transparent;border:0;\n"
-                                          "border-image: url(../graphics/dice1.png);\n"
-                                          "background-repeat:no-repeat;"))
-        self.dice.setText(_fromUtf8(""))
-        self.dice.setObjectName(_fromUtf8("dice"))
         self.title = QtGui.QLabel(Dialog)
         self.title.setGeometry(QtCore.QRect(840, 50, 381, 51))
         self.title.setStyleSheet(
@@ -469,25 +462,36 @@ class Ui_Dialog(object):
         self.title.setText(_fromUtf8(""))
         self.title.setObjectName(_fromUtf8("title"))
         self.startbutton = QtGui.QPushButton(Dialog)
-        self.startbutton.setGeometry(QtCore.QRect(900, 180, 248, 73))
+        self.startbutton.setGeometry(QtCore.QRect(900, 140, 248, 73))
         self.startbutton.setStyleSheet(_fromUtf8("background-color:transparent;border:0;\n"
                                                  "background-image: url(../graphics/start.png);\n"
                                                  "background-repeat:no-repeat;"))
         self.startbutton.setText(_fromUtf8(""))
         self.startbutton.setObjectName(_fromUtf8("startbutton"))
+        self.dice = QtGui.QPushButton(Dialog)
+        self.dice.setGeometry(QtCore.QRect(939, 430, 171, 171))
+        self.dice.setStyleSheet(_fromUtf8("background-color:transparent;border:0;\n"
+                                          "background-image: url(../graphics/dice1.png);\n"
+                                          "background-repeat:no-repeat;"))
+        self.dice.setText(_fromUtf8(""))
+        self.dice.setObjectName(_fromUtf8("dice"))
         self.label_2 = QtGui.QLabel(Dialog)
-        self.label_2.setGeometry(QtCore.QRect(950, 320, 121, 31))
-        self.label_2.setStyleSheet(_fromUtf8("font-size:20px;"))
-        self.label_2.setTextFormat(QtCore.Qt.AutoText)
+        self.label_2.setGeometry(QtCore.QRect(920, 390, 221, 31))
+        self.label_2.setStyleSheet(_fromUtf8("font-size:19px"))
         self.label_2.setObjectName(_fromUtf8("label_2"))
-        self.label_3 = QtGui.QLabel(Dialog)
-        self.label_3.setGeometry(QtCore.QRect(1070, 310, 43, 44))
-        self.label_3.setStyleSheet(_fromUtf8("background-color:transparent;border:0;\n"
+        self.label_4 = QtGui.QLabel(Dialog)
+        self.label_4.setGeometry(QtCore.QRect(950, 240, 111, 41))
+        self.label_4.setStyleSheet(_fromUtf8("font-size:20px"))
+        self.label_4.setObjectName(_fromUtf8("label_4"))
+        self.label_5 = QtGui.QLabel(Dialog)
+        self.label_5.setGeometry(QtCore.QRect(1060, 240, 43, 44))
+        self.label_5.setStyleSheet(_fromUtf8("background-color:transparent;border:0;\n"
                                              "background-image: url(../graphics/counter-red.png);\n"
                                              "background-repeat:no-repeat;"))
-        self.label_3.setText(_fromUtf8(""))
-        self.label_3.setObjectName(_fromUtf8("label_3"))
-        self.label_2.setText(_translate("Dialog", "Kolej gracza:", None))
+        self.label_5.setText(_fromUtf8(""))
+        self.label_5.setObjectName(_fromUtf8("label_5"))
+        self.label_4.setText(_translate("Dialog", "Twój kolor:", None))
+        self.label_2.setText(_translate("Dialog", "Twoja kolej. Rzuć kostką!", None))
 
         LayoutMethodsInserter.insert_methods(self)
 
